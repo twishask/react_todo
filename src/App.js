c2bc71a8af99163ecd4414eed7d5fbab474053fc
Twisha <twishask@gmail.com>
2019-12-15 12:40:36 +0530

    + button for enabling input field

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import './App.css';
 function App() {
 
   const [todoList, setTodoList] = useState([])
+  const [isInputDisabled, setInputDisabled] = useState(true)
+  const [inputValue, setInputValue] = useState("")
 
   const addItem = (e) => {
     if (e.key === "Enter"){
       console.log(e.target.value);
       setTodoList([...todoList, e.target.value])
+      setInputValue("")
     }
+  }
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
   }
 
   return (
     <div className="App">
       <header className="App-header">
+      <h3>Todo</h3>
+      <button onClick={() => setInputDisabled(!isInputDisabled)}>+</button>
       </header>
-      <input type="text" onKeyPress={(e) => addItem(e)} />
+      <input type="text" disabled={isInputDisabled} value={inputValue} onChange={handleInputChange} onKeyPress={addItem} />
       {todoList.map(todo=>{
         return <div>
                 {todo}
